f27cac4f17106a4f16b0b5630c03e1ce0c695c8e
AkXbots <122038135+AkXbots@users.noreply.github.com>
2023-05-21 20:56:34 +0530

    Update afk.py

--- Shikimori/modules/afk.py
+++ Shikimori/modules/afk.py
@@ -188,7 +188,7 @@ dispatcher.add_handler(AFK_REGEX_HANDLER, AFK_GROUP)
 dispatcher.add_handler(NO_AFK_HANDLER, AFK_GROUP)
 dispatcher.add_handler(AFK_REPLY_HANDLER, AFK_REPLY_GROUP)
 
-__mod_name__ = "AFK 💤"
+__mod_name__ = "𝐀ꜰᴋ"
 __help__ = """
 *AFK*
  ❍ `/afk` :Tells other users that you are AFK\n
