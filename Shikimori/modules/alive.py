3fc3b38f33f0aab87f17d5b126e891fd1b73e36f
AkXbots <122038135+AkXbots@users.noreply.github.com>
2023-05-22 12:56:11 +0530

    Update alive.py

--- Shikimori/modules/alive.py
+++ Shikimori/modules/alive.py
@@ -51,15 +51,15 @@ def awake(update: Update, context: CallbackContext):
     TEXT = f"""
     <b>Hi <a href="tg://user?id={user.id}">{first_name}</a>, I'm {bot_name} Robot.
 
-⚪ I'm Working Properly
+⚪ ɪ'ᴍ ᴡᴏʀᴋɪɴɢ ᴘʀᴏᴘᴇʀʟʏ
 
-⚪ My Owner : <a href="https://t.me/{OWNER_USERNAME}">{OWNER_USERNAME}</a></b>
+⚪ ᴍʏ ᴏᴡɴᴇʀ : <a href="https://t.me/{OWNER_USERNAME}">{OWNER_USERNAME}</a></b>
     """
     if NETWORK:
-        TEXT = TEXT + f'\n⚪ <b>I am Powered by : <a href="https://t.me/{NETWORK_USERNAME}">{NETWORK}</a>\n\n' + 'Thanks For Adding Me Here ❤️</b>'
+        TEXT = TEXT + f'\n⚪ <b>ɪ ᴀᴍ ᴘᴏᴡᴇʀᴇᴅ ʙʏ  : <a href="https://t.me/{NETWORK_USERNAME}">{NETWORK}</a>\n\n' + 'ᴛʜᴀɴᴋꜱ ꜰᴏʀ ᴀᴅᴅɪɴɢ ᴍᴇ ʜᴇʀᴇ ❤</b>'
     
     else:
-        TEXT = TEXT + "\n<b>Thanks For Adding Me Here ❤️</b>"
+        TEXT = TEXT + "\n<b>ᴛʜᴀɴᴋꜱ ꜰᴏʀ ᴀᴅᴅɪɴɢ ᴍᴇ ʜᴇʀᴇ ❤</b>"
 
     try:
         if alive_id in ("jpeg", "jpg", "png"):
