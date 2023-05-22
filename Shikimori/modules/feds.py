18e9df4c3ea51583b1da81fbc6602dd9d5566b07
AkXbots <122038135+AkXbots@users.noreply.github.com>
2023-05-22 13:51:57 +0530

    Update feds.py

--- Shikimori/modules/feds.py
+++ Shikimori/modules/feds.py
@@ -2429,7 +2429,7 @@ def fed_user_help(update: Update, context: CallbackContext):
     )
 
 
-__mod_name__ = "Federations"
+__mod_name__ = "𝐅ᴇᴅᴇʀᴀᴛɪᴏɴꜱ"
 
 
 NEW_FED_HANDLER = CommandHandler("newfed", new_fed, run_async=True)
