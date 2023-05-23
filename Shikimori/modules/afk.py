b5fda1157b23e1009508d122b620642eb1462f0b
AkXbots <122038135+AkXbots@users.noreply.github.com>
2023-05-23 13:29:11 +0530

    Update afk.py

--- Shikimori/modules/afk.py
+++ Shikimori/modules/afk.py
@@ -33,9 +33,9 @@ from telegram.ext import Filters, MessageHandler
 from Shikimori import dispatcher
 from Shikimori.modules.disable import DisableAbleCommandHandler, DisableAbleMessageHandler
 from Shikimori.modules.redis.afk_redis import start_afk, end_afk, is_user_afk, afk_reason
-from Shikimori import REDIS
 from Shikimori.modules.users import get_user_id
-
+ 
+    
 from Shikimori.modules.helper_funcs.alternate import send_message
 from Shikimori.modules.helper_funcs.readable_time import get_readable_time
 
